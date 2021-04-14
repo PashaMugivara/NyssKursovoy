37ce31dc01bc768f193e13b6d9c5f8684878ef48
PashaMugivara <65304959+PashaMugivara@users.noreply.github.com>
2021-04-14 04:06:17 +0300

    Initial commit

---
+++ NyssKursovoy/Controllers/FilesController.cs
@@ -0,0 +1,90 @@
+﻿
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NyssKursovoy.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace NyssKursovoy.Controllers
+{
+    public class FilesController : Controller
+    {
+        private IHostingEnvironment _env;
+        public FilesController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        public IActionResult List()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult List(IFormFile shifrFile, string action, string whereShifr, string key, string Content)
+        {
+            Text.Key = key;
+            Text.InFile = Content;
+            if(action == "Загрузить файл") 
+            {
+                if (shifrFile != null && shifrFile.Length > 0)
+                {
+                    if (shifrFile.FileName.Substring(shifrFile.FileName.Length - 4) == "docx")
+                    {
+                        string fullpath = Path.Combine(_env.WebRootPath, "Files/input.docx");
+                        using (var fileStream = new FileStream(fullpath, FileMode.Create))
+                        {
+                            shifrFile.CopyToAsync(fileStream);
+                            fileStream.Close();
+
+                        }
+                        Text.InFile = Text.TextFromWord("wwwroot/Files/input.docx");
+                    }
+                    else if (shifrFile.FileName.Substring(shifrFile.FileName.Length - 3) == "txt")
+                    {
+                        string fullpath = Path.Combine(_env.WebRootPath, "Files/input.txt");
+                        using (var fileStream = new FileStream(fullpath, FileMode.Create))
+                        {
+                            shifrFile.CopyToAsync(fileStream);
+                            fileStream.Close();
+
+                        }
+                        using (StreamReader sr = new StreamReader("wwwroot/Files/input.txt"))
+                        {
+                            Text.InFile = sr.ReadToEnd();
+                            sr.Close();
+                        }
+                    }
+                }
+            }
+            if (action == "Преобразовать")
+            {
+                if (whereShifr == "Шифровать") Text.OutFile = Text.Encrypt();
+                if (whereShifr == "Дешифровать") Text.OutFile = Text.Decrypt();
+            }
+            if (action == "Сохранить в файл")
+            {
+
+                string fullpath = Path.Combine(_env.WebRootPath, "Files/output.txt");
+                using (var fileStream = new StreamWriter(fullpath, false))
+                {
+                    fileStream.Write(Text.OutFile);
+                    fileStream.Close();
+
+                }
+            }
+
+            return RedirectToAction("List");
+        }
+
+
+
+    }
+}
---
+++ NyssKursovoy/Data/Models/Text.cs
@@ -0,0 +1,119 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace NyssKursovoy.Data.Models
+{
+    public static class Text
+    {
+        private static Dictionary<char, int> letters = new Dictionary<char, int>() {
+            {'а',1 }, {'б',2 },  {'в',3 }, {'г',4 }, {'д',5 }, {'е',6 }, {'ё',7 }, {'ж',8 }, {'з',9 }, {'и',10 },
+            {'й',11 }, {'к',12 },  {'л',13 }, {'м',14 }, {'н',15 }, {'о',16 }, {'п',17 }, {'р',18 }, {'с',19 }, {'т',20 },
+            {'у',21 }, {'ф',22 },  {'х',23 }, {'ц',24 }, {'ч',25 }, {'ш',26 }, {'щ',27 }, {'ъ',28 }, {'ы',29 }, {'ь',30 },
+            {'э',31 }, {'ю',32 },  {'я',33 }//зато не надо мучиться с ё
+        };
+        public static string OutFile { get; set; } = "";
+        public static string InFile { get; set; } = "";
+        public static string Key { get; set; } = "Скорпион";
+        private static Queue<char> GetRepeatKey(string s, int n)
+        {
+            Queue<char> k = new Queue<char>() { };
+            var p = s;
+            while (p.Length < n)
+            {
+                p += p;
+            }
+            p= p.Substring(0, n);
+            foreach (var ir in p) { k.Enqueue(char.ToLower(ir)); }
+            return k;
+        }
+
+        public static string Decrypt()
+        {
+            bool isUpper = false;
+            string c = InFile;
+            Queue<char> k = GetRepeatKey(Key, c.Length);
+            string p="";
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (char.IsUpper(c[i])) isUpper = true;
+                if (letters.ContainsKey(char.ToLower(c[i]))) 
+                {
+                    int a = (letters[char.ToLower(c[i])] + 33 - letters[k.Dequeue()]) % 33 + 1;
+                    foreach (var ir in letters) 
+                        if (ir.Value == a) 
+                        {
+                            if (isUpper) p += char.ToUpper(ir.Key).ToString();
+                            else p += ir.Key.ToString();
+                            break;
+                        }
+                }
+                else p += c[i].ToString();
+                isUpper = false;
+            }
+            return p;
+        }
+        public static string Encrypt()
+        {
+            bool isUpper = false;
+            string p = InFile;
+            Queue<char> k = GetRepeatKey(Key, p.Length);
+            string c = "";
+            for (int i = 0; i < p.Length; i++)
+            {
+                if (char.IsUpper(p[i])) isUpper = true;
+                if (letters.ContainsKey(char.ToLower(p[i])))
+                {
+                    int a = (letters[char.ToLower(p[i])] + letters[k.Dequeue()] - 2) % 33 + 1;
+                    foreach (var ir in letters)
+                        if (ir.Value == a)
+                        {
+                            if (isUpper) c += char.ToUpper(ir.Key).ToString();
+                            else c += ir.Key.ToString();
+                            break;
+                        }
+                }
+                else c += p[i].ToString();
+                isUpper = false;
+            }
+            return c;
+        }
+        public static string TextFromWord(string file)
+        {
+            const string wordmlNamespace = "http://schemas.openxmlformats.org/wordprocessingml/2006/main";
+
+            StringBuilder textBuilder = new StringBuilder();
+            using (WordprocessingDocument wdDoc = WordprocessingDocument.Open(file, false))
+            {
+                // Manage namespaces to perform XPath queries.  
+                NameTable nt = new NameTable();
+                XmlNamespaceManager nsManager = new XmlNamespaceManager(nt);
+                nsManager.AddNamespace("w", wordmlNamespace);
+
+                // Get the document part from the package.  
+                // Load the XML in the document part into an XmlDocument instance.  
+                XmlDocument xdoc = new XmlDocument(nt);
+                xdoc.Load(wdDoc.MainDocumentPart.GetStream());
+
+                XmlNodeList paragraphNodes = xdoc.SelectNodes("//w:p", nsManager);
+                foreach (XmlNode paragraphNode in paragraphNodes)
+                {
+                    XmlNodeList textNodes = paragraphNode.SelectNodes(".//w:t", nsManager);
+                    foreach (System.Xml.XmlNode textNode in textNodes)
+                    {
+                        textBuilder.Append(textNode.InnerText);
+                    }
+                    textBuilder.Append(Environment.NewLine);
+                }
+
+            }
+            return textBuilder.ToString();
+        }
+    }
+}
